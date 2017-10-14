1b5c348c64a80e67b1389f605d9fc5055a9c24bf
simonvannarath <vvidfinishing@gmail.com>
2017-10-14 18:00:12 +1100

    Step 4 complete

--- Animal.cs
+++ Animal.cs
@@ -9,7 +9,11 @@ namespace Animal_Inheritance
 		static void Main(string[] args)
 		{
 			Animal cat = new Cat();
-			Console.WriteLine(cat.Talk());
+			Animal dog = new Dog();
+
+			Console.WriteLine("A cat says: " + cat.Talk());
+			Console.WriteLine("A dog says: " + dog.Talk());
+
 			Console.ReadLine();
 		}
 
@@ -32,5 +36,115 @@ namespace Animal_Inheritance
 		}
 	}
 
+	class Dog : Animal
+	{
+		public Dog()
+		{
+			greeting = "Bark!";
+		}
+	}
 
+	class Bird : Animal
+	{
+		public Bird()
+		{
+			greeting = "Chirp!";
+		}
+	}
+
+	class Sheep : Animal
+	{
+		public Sheep()
+		{
+			greeting = "Baa!";
+		}
+	}
+
+	class Pig : Animal
+	{
+		public Pig()
+		{
+			greeting = "Oink!";
+		}
+	}
+
+	class Cow : Animal
+	{
+		public Cow()
+		{
+			greeting = "Moo!";
+		}
+	}
+
+	class Horse : Animal
+	{
+		public Horse()
+		{
+			greeting = "Neigh!";
+		}
+	}
+
+	class Fish : Animal
+	{
+		public Fish()
+		{
+			greeting = "Bloop!";
+		}
+	}
+
+	class Lion : Animal
+	{
+		public Lion()
+		{
+			greeting = "Roar!";
+		}
+	}
+
+	class Bee : Animal
+	{
+		public Bee()
+		{
+			greeting = "Bzzz!";
+		}
+	}
+
+	class Frog : Animal
+	{
+		public Frog()
+		{
+			greeting = "Ribbit!";
+		}
+	}
+
+	class Duck : Animal
+	{
+		public Duck()
+		{
+			greeting = "Quack!";
+		}
+	}
+
+	class Jellyfish : Animal
+	{
+		public Jellyfish()
+		{
+			greeting = "...";
+		}
+	}
+
+	class Lad : Animal
+	{
+		public Lad()
+		{
+			greeting = "U wot kient?!";
+		}
+	}
+
+	class Owen : Animal
+	{
+		public Owen()
+		{
+			greeting = "Waaaaowww!";
+		}
+	}
 }
